e9d932741a72460e89f31d1183b7ccd64c813c19
Dima_V <astrallux777@ukr.net>
2025-04-29 22:52:09 +0300

    29.04.2025-DZ-05-02

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,58 @@
 'use strict';
 
+const getTotalBalanceByGender = (users, gender) => {
+  const sums = users.filter(user => user.gender === gender);
+  // console.log(sums);
 
+  const total = sums.map(sum => sum.balance);
+  // console.log(total);
+
+  const result = total.reduce((acum, number) => {
+    return acum + number;
+  }, 0);
+  // console.log(result);
+
+  return result;
+};
+
+const clients = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+
+console.log(getTotalBalanceByGender(clients, 'female')); // 8863
